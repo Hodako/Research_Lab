302e6bfcca175ba94950b35e1a3c8d924241f5e9
Azizul Hakim <114043543+Hodako@users.noreply.github.com>
2025-02-01 16:12:41 +0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ResponsiveResearchPlatform from './components/ResponsiveResearchPlatform';
+
+const App = () => {
+  return <ResponsiveResearchPlatform />;
+};
+
+export default App;
