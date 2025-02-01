f7409f9c644ef149fd00ee00211cdbb029b2a85a
Azizul Hakim <114043543+Hodako@users.noreply.github.com>
2025-02-01 20:06:24 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Upload from './components/Upload';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/upload" component={Upload} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
